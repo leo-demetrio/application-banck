370c09d1c2863004997ab50e55fd127d58129897
leo-demetrio <leocdemetrio@gmail.com>
2022-05-26 19:15:16 -0300

    Operators finished

--- src/program/BankAccount.java
+++ src/program/BankAccount.java
@@ -9,11 +9,41 @@ public class BankAccount {
     private Client client;
     private Double balance = 0D;
 
+    public void deposit(Double value){
+        if(value > 0){
+            setBalance(value + balance);
+            System.out.println("Deposit success");
+        }else {
+            System.out.println("Deposit fail");
+        }
+    }
+    public void withdraw(Double value){
+        if(value > 0 && this.balance >= value){
+            setBalance(getBalance() - value);
+            System.out.println("Withdraw success");
+        }else {
+            System.out.println("Withdraw fail");
+        }
+    }
+    public void transfer(BankAccount clientDeposit,Double value){
+        if(value > 0 && this.balance >= value){
+            setBalance(getBalance() - value);
+            clientDeposit.setBalance(clientDeposit.getBalance() + value);
+            System.out.println("Count sender: " + this.client.getName());
+            System.out.println("Count recipient: " + clientDeposit.client.getName());
+            System.out.println("Balance recipient: " + clientDeposit.getBalance());
+            System.out.println("Balance sender: " + this.getBalance());
+            System.out.println("Transfer success");
+        }else {
+            System.out.println("Transfer fail");
+        }
+    }
     public BankAccount(Client client) {
         this.numberAccount = counterAccount;
         this.client = client;
         counterAccount += 1;
     }
+
 
     public int getNumberAccount() {
         return numberAccount;
@@ -43,6 +73,6 @@ public class BankAccount {
                 "\nNome: " + this.client.getName() +
                 "\nEmail: " + this.client.getEmail() +
                 "\nEmail: " + this.client.getCpf() +
-                "\nEmail: " + NumberFormat.doubleToString(this.getBalance()) + "\n";
+                "\nBalance: " + NumberFormat.doubleToString(this.getBalance()) + "\n";
     }
 }
